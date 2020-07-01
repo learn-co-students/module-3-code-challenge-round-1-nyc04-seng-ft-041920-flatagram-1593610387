f8fbd1958032066b2faf2131453e85a1c38fc8dd
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-01 11:31:14 -0400

    downvote button working // and dislikes persisting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const h2Tag = document.querySelector(".title")
 const imgTag = document.querySelector(".image")
-const likesSection = document.querySelector(".likes-section")
+const btnContainer = document.querySelector(".btnContainer")
 const likes = document.querySelector(".likes")
 // trust me on this
 const card = document.querySelector(".image-container")
@@ -23,17 +23,20 @@ form.addEventListener('submit', e => {
   form.reset();
 })
 
+function postComments(item) {
+
+}
+
 function renderImage(item) {
   card.dataset.id = item.id
   h2Tag.textContent = item.title
   imgTag.src = item.image
   likes.textContent = `${item.likes} likes`
 
-  likesSection.addEventListener('click', e => {
-
-    likes.textContent = `${item.likes + 1} likes`
-    if (e.target.className === 'like-button')
-      e.preventDefault();{
+  btnContainer.addEventListener('click', e => {
+    if (e.target.id === 'like') {
+      e.preventDefault();
+      likes.textContent = `${item.likes + 1} likes`
       // get your patch going here
       fetch(`http://localhost:3000/images/${item.id}`, {
           method: 'PATCH',
@@ -41,6 +44,18 @@ function renderImage(item) {
             "Content-Type": "application/json"
           },
           body: JSON.stringify({likes: item.likes + 1})
+        }
+      ).then(resp => resp.json()).then(updatedLikes => {
+        item.likes = updatedLikes.likes
+      })
+    } else if (e.target.id === 'downvote') {
+      likes.textContent = `${item.likes - 1} likes`
+      fetch(`http://localhost:3000/images/${item.id}`, {
+          method: 'PATCH',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({likes: item.likes - 1})
         }
       ).then(resp => resp.json()).then(updatedLikes => {
         item.likes = updatedLikes.likes
@@ -74,3 +89,4 @@ function comments(item, ) {
 // does it keep refreshing cause it's in the function to render it from the fetch
 // nope need to ask why it keeps refreshing :(
 // if i take it out of this function, i'll get to use the dataset to find the exact element it's doing this for
+// it refreshie bc live server :(
