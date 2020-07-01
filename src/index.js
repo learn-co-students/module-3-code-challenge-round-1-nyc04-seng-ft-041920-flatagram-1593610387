4c39019fda6bf4850ab6012a56715dc00d458f4d
residenttrickster <metkusblackus@gmail.com>
2020-07-01 11:29:14 -0400

    90 min 2 deliverables done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
 // write your code here
+
+//Dom Elements
+const dogContainer = document.querySelector(".image-container");
+const commentForm = document.querySelector(".comment-form");
+
+//Event Listeners
+dogContainer.addEventListener("click", function(event) {
+
+    if(event.target.matches(".like-button")) { 
+    const dogCard = event.target.closest(".image-card")
+    // const listingId = dogCard.dataset.id
+    const likesSpan = dogCard.querySelector(".likes")
+
+    const newLikes = parseInt(likesSpan.textContent) + 1
+
+    likesSpan.textContent = `${newLikes}`
+
+    fetch('http://localhost:3000/images/1', {
+    method: 'PATCH',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({likes: newLikes}),
+    })
+    }
+});
+
+// commentForm.addEventListener("submit", function(event) {
+//     event.preventDefault()
+
+//     const newComment = {
+//         content: event.target.content.value
+//     }
+
+//     fetch('http://localhost:3000/comments', {
+//     method: 'POST', 
+//     headers: {
+//     'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newComment),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//     console.log('Success:', data);
+//     })
+//     .catch((error) => {
+//     console.error('Error:', error);
+//     });
+// }); 
+
+    
+//Initial Render 
+fetch('http://localhost:3000/images/1')
+    .then(response => response.json())
+    .then(dogObject => {
+        dogContainer.innerHTML = `<div class="image-card" data-id=${dogObject.id}>
+        <h2 class="title">${dogObject.title}</h2>
+        <img src="${dogObject.image}" class="image" />
+        <div class="likes-section">
+        <span class="likes">${dogObject.likes} likes</span>
+        <button class="like-button">♥</button>
+        </div>
+        <ul class="comments">
+        <li>${dogObject.comments[0].content}</li>
+        <li>${dogObject.comments[1].content}</li>
+        <li>${dogObject.comments[2].content}</li>
+    </ul>
+    <form class="comment-form">
+        <input
+        class="comment-input"
+        type="text"
+        name="comment"
+        placeholder="Add a comment..."
+        />
+        <button class="comment-button" type="submit">Post</button>
+    </form>`
+        });
