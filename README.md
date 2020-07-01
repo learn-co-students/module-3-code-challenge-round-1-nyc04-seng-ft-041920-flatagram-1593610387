03447182756514ca1a1052b83e20415b559b6c52
amer519 <afahmy519@gmail.com>
2020-07-01 11:50:51 -0400

    core deliverables

--- README.md
+++ README.md
@@ -29,9 +29,9 @@ The endpoints you will need are:
 
 As a user, I can:
 
-- See the image received from the server, including its title, likes and comments when the page loads
-- Click on the heart icon to increase image likes, and still see them when I reload the page
-- Add a comment (no persistance needed)
+XX- See the image received from the server, including its title, likes and comments when the page loads
+XX- Click on the heart icon to increase image likes, and still see them when I reload the page
+XX- Add a comment (no persistance needed)
 
 ## Advanced Deliverables
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,93 @@
-// write your code here
+//DOM
+
+const viewImage = document.querySelector(".image")
+const imgTitle = document.querySelector(".title")
+const imgLikes = document.querySelector(".likes")
+const imgComments = document.querySelector(".comments")
+const likeDiv = document.querySelector(".likes-section")
+const imgCommentForm = document.querySelector(".comment-form")
+
+
+
+
+//Fetch for image
+fetch('http://localhost:3000/images')
+.then(response => response.json())
+.then(imgData => {
+    imgData.forEach(imgObj =>{
+        
+        renderImage(imgObj)
+    })
+})
+// render pic, title, likes
+function renderImage(imgObj) {
+    
+    viewImage.src = `${imgObj.image}` 
+    
+    imgTitle.innerHTML = `
+    ${imgObj.title}
+    `
+    imgLikes.innerHTML = `
+    ${imgObj.likes}
+    `
+    //add likes feature and persist in database
+    const likeButton = likeDiv.querySelector(".like-button")
+    
+    likeButton.addEventListener("click", () => {
+    imgObj.likes++
+    imgLikes.textContent = `${imgObj.likes}`
+
+    fetch(`http://localhost:3000/images/${imgObj.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            'likes': imgObj.likes
+        })
+
+    })
+
+    })
+}
+
+// Fetch for comments
+fetch('http://localhost:3000/comments')
+.then(response => response.json())
+.then(imgComments => {
+    imgComments.forEach(imgComment =>{
+        
+        renderComment(imgComment)
+    })
+})
+// render comments
+function renderComment(imgComment) {
+    imgComments.innerHTML = `
+    ${imgComment.content}
+    `
+}
+
+//add comment
+imgCommentForm.addEventListener("submit", event => {
+    event.preventDefault()
+    // debugger
+    const post = event.target.comment.value
+
+    fetch('http://localhost:3000/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify ({
+            "content": post
+        })
+    })
+    .then(response => response.json())
+    .then(imgComment => {
+        renderComment(imgComment)
+    })
+})
+
+
+
