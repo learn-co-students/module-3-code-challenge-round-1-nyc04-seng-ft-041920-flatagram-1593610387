c6a473dda9080fcd907bc6802fff39c3166d1d69
stephepush <7speter@gmail.com>
2020-07-01 12:09:42 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ window.addEventListener("load", (event) =>{
                 const containerElementAttr = containerElement.setAttribute("data-id", `${post.id}`)
                 containerElement.append(containerElementAttr)
                 const postTitle = document.querySelector('.title');
-                
+                //tried inserting id from each instance of data but couldn't figure it out. Rest of code
+                // what I would do if this all worked.
                 //postTitleID = setAttribute("value", `${post.id}`);
                 //postTitle.postTitleID
                 postTitle = postTitle.innerText = `${post.title}`;
@@ -31,7 +32,26 @@ window.addEventListener("load", (event) =>{
                     const commentInput = event.target.comment.value
                     fetch("http://localhost:3000/comments", {
                         method: 'POST',
-                        
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }, 
+                        body: JSON.stringify({
+                            comment: commentInput
+                        })
+                            .then(response => response.json())
+                            .then(commentInput => {
+
+                            })
+
+                
+                    })
+                    commentButton.addEventListnener("click", (event) => {
+                         const comments
+                        /*
+                            clear ul element of content, foreach each comment POST (add each new one with
+                                += when assigning innerHTML with appropriate string interpolation) invoke 
+                                this function on line 42 to render each submitted comment to the frontend
+                        */       
                     })
                 })
             })
